1cd240c4defd82ec26ee94ce9ab8f4f2a3337ae3
inflexjs <helidanger@yandex.ru>
2022-01-13 17:48:17 +0300

    fetch placeholder

--- js/script.js
+++ js/script.js
@@ -285,4 +285,16 @@ document.addEventListener("DOMContentLoaded", () => {
       closeModal();
     }, 4000);
   }
+
+  fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: "POST",
+      body: JSON.stringify({
+        name: 'Alex'
+      }),
+      headers: {
+        'Content-type': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(json => console.log(json));
 });
