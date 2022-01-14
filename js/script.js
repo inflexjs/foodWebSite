45d2a5bcd73ccabe2fa4fcc5332cc3dbdd3f3d48
inflexjs <helidanger@yandex.ru>
2022-01-14 09:44:42 +0300

    getResource();Errors

--- js/script.js
+++ js/script.js
@@ -159,50 +159,72 @@ document.addEventListener("DOMContentLoaded", () => {
 
       this.classes.forEach(className => element.classList.add(className));
       element.innerHTML = `
-        <img src=${this.src} alt=${this.alt}>
-        <h3 class="menu__item-subtitle">${this.title}</h3>
-        <div class="menu__item-descr">${this.descr}</div>
-        <div class="menu__item-divider"></div>
-        <div class="menu__item-price">
-            <div class="menu__item-cost">Цена:</div>
-            <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-        </div>
+          <img src=${this.src} alt=${this.altimg}>
+          <h3 class="menu__item-subtitle">${this.title}</h3>
+          <div class="menu__item-descr">${this.descr}</div>
+          <div class="menu__item-divider"></div>
+          <div class="menu__item-price">
+              <div class="menu__item-cost">Цена:</div>
+              <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+          </div>
       `;
 
       this.parent.append(element);
     }
   }
 
-  new CardMenu(
-    "img/tabs/vegy.jpg",
-    "vegy",
-    'Меню "Фитнес"',
-    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов.Продукт активных и здоровых людей.Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-    14,
-    '.menu .container',
-    'menu__item',
-    'big',
-  ).render();
+  const getResource = async (url) => {
+    const res = await fetch(url);
 
-  new CardMenu(
-    "img/tabs/elite.jpg",
-    "elite",
-    'Меню “Премиум”',
-    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд.Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-    21,
-    '.menu .container',
-    'menu__item',
-    'big',
-  ).render();
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status ${res.status}`);
+    }
 
-  new CardMenu(
-    "img/tabs/post.jpg",
-    "post",
-    'Меню "Постное"',
-    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-    9,
-    '.menu .container',
-  ).render();
+    return await res.json();
+  };
+
+  // getResource("http://localhost:3000/menu")
+  //   .then(data => {
+  //     data.forEach(({
+  //       img,
+  //       altimg,
+  //       title,
+  //       descr,
+  //       price
+  //     }) => {
+  //       new CardMenu(img, altimg, title, descr, price, '.menu .container', 'menu__item').render();
+  //     })
+  //   })
+
+  getResource("http://localhost:3000/menu")
+    .then(data => createCard(data));
+
+  function createCard(data) {
+    data.forEach(({
+      img,
+      altimg,
+      title,
+      descr,
+      price
+    }) => {
+      const element = document.createElement("div");
+
+      element.classList.add("menu__item");
+
+      element.innerHTML = `
+        <img src=${img} alt=${altimg}>
+        <h3 class="menu__item-subtitle">${title}</h3>
+        <div class="menu__item-descr">${descr}</div>
+        <div class="menu__item-divider"></div>
+        <div class="menu__item-price">
+            <div class="menu__item-cost">Цена:</div>
+            <div class="menu__item-total"><span>${price}</span> грн/день</div>
+        </div>
+      `;
+
+      document.querySelector(".menu .container").append(element);
+    });
+  }
 
   // Forms
   const forms = document.querySelectorAll("form");
@@ -245,18 +267,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
       const formData = new FormData(form);
 
-      const object = {};
-      formData.forEach(function (value, key) {
-        object[key] = value;
-      });
+      const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-      const json = JSON.stringify(object);
-
-      postData('http://localhost:3000/requests', JSON.stringify(object))
+      postData('http://localhost:3000/requests', json)
         .then(data => {
           console.log(data);
           showThanksModal(message.success);
-          form.reset();
           statusMessage.remove();
         })
         .catch(() => {
